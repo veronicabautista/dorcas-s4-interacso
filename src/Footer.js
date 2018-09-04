7deca4694c975fb79334b71f947e138bcf4078c9
Marta Ocaña <marta.o.padilla@gmail.com>
2018-09-04 21:42:20 +0200

    Positioned elements in header and footer. No margins and no styles yet.

--- src/Footer.js
+++ src/Footer.js
@@ -4,11 +4,11 @@ class Footer extends React.Component {
   render(){
      return (
       <div className="footer__container">
-        <div className="footer__notif-number"></div>
+        <div className="footer__notif-number">NOTIFICACIONES (3)</div>
         <div className="footer__notif-details">
-          <div className="detail__notif-category"></div>
-          <div className="detail__notif-content"></div>
-          <div className="detail__notif-time"></div>
+          <div className="detail__notif-category">Nueva tarea</div>
+          <div className="detail__notif-content">Flecha: borrar artista</div>
+          <div className="detail__notif-time">Hace 20 minutos</div>
         </div>
       </div>
     );
