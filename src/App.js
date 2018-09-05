1ea2cf51ec981ec7795c1f42803ba5946e5ce00f
Veronica Bautista <veronicabautista87@gmail.com>
2018-09-05 22:02:36 +0200

    updated styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
 import Projects from './components/Projects';
+import Footer from './components/Footer';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="container__all">
-        <Projects />
+      <div className="App">
+        <Projects />    
       </div>
     );
   }
