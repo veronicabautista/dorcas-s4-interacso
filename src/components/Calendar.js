628a4b64730f6326a906a2aee44fa0572d587d74
Imoa1991 <ordonez.im@gmail.com>
2018-09-05 21:26:28 +0200

    resolved bug in js dates

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -8,13 +8,17 @@ class Calendar extends React.Component {
     super(props)
     this.texts = {
       title: "Calendar"
-
     }
+    this.state = {
+      calendarNotifications: []
+    }
+    this.getCalendarNotifications();
   }
   makeCalendarStructure() {
     let startDate = this.calculateStartDate();
     let weeks = [];
     let dateToPrint = 0;
+    let dateInMs = 0;
     for (let i = 0; i < 4; i++) {
       let days = [];
       for (let j= 0; j < 5; j++) {
@@ -22,36 +26,47 @@ class Calendar extends React.Component {
         days.push(
           <td className="calendar__day">
             <h2 className="callendar__day--number">{dateToPrint}</h2>
+            {this.searchNotificationsByDate(startDate)}
           </td>
         );
-        startDate.setDate(dateToPrint + 1);
+        startDate = this.incrementDaysInMiliseconds(startDate, 1);
       }
-      startDate.setDate(dateToPrint + 2); // Para que entre en lunes en la siguiente vuelta, realmente ya estoy en sabado cuando termino el bucle.
+      startDate = this.incrementDaysInMiliseconds(startDate, 2); // Para que entre en lunes en la siguiente vuelta, realmente ya estoy en sabado cuando termino el bucle.
+      console.log(startDate);
       weeks.push(<tr className='calendar__week'>{days}</tr>);
     }
-    this.getCalendarNotifications();
     return <table className="table__calendar"><tbody>{weeks}</tbody></table>;
   }
-    getCalendarNotifications() {
-      fetch(
-        this.props.apiService + 'calendar',
-        {
-          method: 'get',
-          withCredentials: true,
-          headers: {
-            'Cache-Control': 'no-cache',
-            'Authorization': Env.token,
-            'Content-Type': 'application/json'
-          }
+  getCalendarNotifications() {
+    fetch(
+      this.props.apiService + 'calendar',
+      {
+        method: 'get',
+        withCredentials: true,
+        headers: {
+          'Cache-Control': 'no-cache',
+          'Authorization': Env.token,
+          'Content-Type': 'application/json'
         }
-      ).then(response => {
-        return response.json();
       }
-    ).then(json => {
-      console.log('Funciono pero devuelvo un 204...')
-      console.log(json);
-    });
+    ).then(response => {
+      return response.json();
     }
+  ).then(json => {
+    console.log(json);
+    this.setState({
+      calendarNotifications: json
+    });
+  });
+}
+incrementDaysInMiliseconds(date, numDays) {
+  let milisecondsInADay = 86400000;
+  let totalMiliseconds = milisecondsInADay * numDays;
+  return new Date(date.getTime() + totalMiliseconds);
+}
+searchNotificationsByDate(date) {
+  let dateFormatted = date.getFullYear() + "-" + date.getMonth() + "-" + date.getDate();
+}
 calculateStartDate() {
   let today = new Date();
   let milisecondsInADay = 86400000;
@@ -61,17 +76,16 @@ calculateStartDate() {
   let startDate = new Date(miliseconds);
   return startDate; //objeto con el día de inicio
 }
-  render() {
-    console.log(Env.token);
-    console.log('hola estoy funcionando')
-    return (
-      <React.Fragment>
-        <Header title={this.texts.title}/>
-          {this.makeCalendarStructure()}
-        <Footer />
-      </React.Fragment>
-      );
-  }
+render() {
+  console.log(Env.token);
+  console.log('hola estoy funcionando')
+  return (
+    <React.Fragment>
+      <Header title={this.texts.title}/>
+      {this.makeCalendarStructure()}
+    </React.Fragment>
+  );
+}
 }
 
 export default Calendar;
