5c1eb3d4ddabbd6b1dc78bc7fd9bcda5f578c8b7
Albametnet <alhernandezsan@gmail.com>
2018-09-05 12:16:40 +0200

    fixing conflicts

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          
+        <header className="App-header">  
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
