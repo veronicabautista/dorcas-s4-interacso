01e412eb58c1c7734e529e7987bb96a383f1f5c4
Albametnet <alhernandezsan@gmail.com>
2018-09-06 08:37:00 +0200

    added some details on team

--- src/components/Team.js
+++ src/components/Team.js
@@ -13,8 +13,8 @@ class Team extends React.Component {
           <div className="dashborad chart__commits"><p className="commits-title">Commits Semana</p></div>
           <div className="dashborad average__container-commits"><p className="commits-number">5</p><p className="commits-text">Commits/dia/persona</p></div>
           <div className="dashborad average__container-tasks"><p className="tasks-number">4</p><p className="tasks-text">Tareas/dia/persona</p></div>
-          <div className="dashborad people__container-asana"><p className="asana-title">Asana killer</p></div>
-          <div className="dashborad people__container-git"><p className="git-title">Git killer</p></div>
+          <div className="dashborad people__container-asana"><p className="asana-title">Asana killer</p><p className="killer-name">Emilio de la Torre</p><p className="killer-record">145</p><p className="killer-detail">Tareas completadas esta semana</p></div>
+          <div className="dashborad people__container-git"><p className="git-title">Git killer</p><p className="killer-name">Ruben Sarrió</p><p className="killer-record">305</p><p className="killer-detail">Commits esta semana</p></div>
         </div>
         <Footer />
       </div>
