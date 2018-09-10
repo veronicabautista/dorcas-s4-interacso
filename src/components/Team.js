654c24376386b7cc2c50c54cbfe149bb3bb496e7
Marta Ocaña <marta.o.padilla@gmail.com>
2018-09-10 19:59:27 +0200

    Finished killercharts functionality

--- src/components/Team.js
+++ src/components/Team.js
@@ -42,6 +42,7 @@ class Team extends React.Component {
       }
     ).then(json => {
       this.getTasksWinner(json);
+      this.getCommitsWinner(json);
     }).catch(error => {
       alert("El token es incorrecto");
       console.error(error);
@@ -65,17 +66,56 @@ class Team extends React.Component {
     });
   }
 
+  getCommitsWinner(json) {
+    let maxCommits = 0;
+    let winnerCommitsObj = {};
+    json.data.map(peopleData => {
+      if (peopleData.commits > maxCommits) {
+        maxCommits = peopleData.commits;
+        winnerCommitsObj = peopleData;
+      }
+    });
+    this.setState({
+      commitsWinner: winnerCommitsObj,
+    });
+  }
+
   render() {
     return (
       <div className="team__container databoard">
         <Header title={this.texts.title} />
         <div className="main__container-team">
-          <div className="dashborad chart__tasks"><p className="tasks-title">Tareas Semana</p><div className="tasks-pic"></div></div>
-          <div className="dashborad chart__commits"><p className="commits-title">Commits Semana</p><div className="commits-pic"></div></div>
-          <div className="dashborad average__container-commits"><p className="commits-number">5</p><p className="commits-text">Commits/dia/persona</p></div>
-          <div className="dashborad average__container-tasks"><p className="tasks-number">4</p><p className="tasks-text">Tareas/dia/persona</p></div>
-          <div className="dashborad people__container-asana"><p className="asana-title">Asana killer</p><div className="profile-pic"></div><p className="killer-name">John Doe</p><p className="killer-record">145</p><p className="killer-detail">Tareas completadas esta semana</p></div>
-          <div className="dashborad people__container-git"><p className="git-title">Git killer</p><div className="profile-pic"></div><p className="killer-name">John Doe</p><p className="killer-record">305</p><p className="killer-detail">Commits esta semana</p></div>
+          <div className="dashborad chart__tasks">
+            <p className="tasks-title">Tareas Semana</p>
+            <div className="tasks-pic">
+            </div>
+          </div>
+          <div className="dashborad chart__commits">
+            <p className="commits-title">Commits Semana</p>
+            <div className="commits-pic"></div>
+          </div>
+          <div className="dashborad average__container-commits">
+            <p className="commits-number">5</p>
+            <p className="commits-text">Commits/dia/persona</p>
+          </div>
+          <div className="dashborad average__container-tasks">
+            <p className="tasks-number">4</p>
+            <p className="tasks-text">Tareas/dia/persona</p>
+          </div>
+          <div className="dashborad people__container-asana">
+            <p className="asana-title">Asana killer</p>
+            <img className="profile-pic" src={this.state.tasksWinner.photo}></img>
+            <p className="killer-name">{this.state.tasksWinner.nombre}</p>
+            <p className="killer-record">{this.state.tasksWinner.tasks}</p>
+            <p className="killer-detail">Tareas completadas esta semana</p>
+          </div>
+          <div className="dashborad people__container-git">
+            <p className="git-title">Git killer</p>
+            <img className="profile-pic" src={this.state.commitsWinner.photo}></img>
+            <p className="killer-name">{this.state.commitsWinner.nombre}</p>
+            <p className="killer-record">{this.state.commitsWinner.commits}</p>
+            <p className="killer-detail">Commits esta semana</p>
+          </div>
         </div>
         <Notifications />
       </div>
